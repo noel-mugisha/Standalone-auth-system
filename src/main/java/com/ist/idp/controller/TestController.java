1cb3de10e345e67b3e1ed0ad0c619eeca1d3c3a7
Noel <noelmugisha332@gmail.com>
2025-09-05 11:55:52 +0200

    Update TestController to return ApiMessageResponse in sayHello method

--- src/main/java/com/ist/idp/controller/TestController.java
+++ src/main/java/com/ist/idp/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ist.idp.controller;
 
+import com.ist.idp.dto.response.ApiMessageResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import java.security.Principal;
 @RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/hello")
-    public ResponseEntity<String> sayHello(Principal principal) {
-        return ResponseEntity.ok("Hello, " + principal.getName());
+    public ResponseEntity<?> sayHello(Principal principal) {
+        return ResponseEntity.ok(new ApiMessageResponse("Hello, " + principal.getName()));
     }
 }
