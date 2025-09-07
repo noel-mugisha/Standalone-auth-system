01da1e62f101fd2463fc4d443cd033f749d95784
Noel <noelmugisha332@gmail.com>
2025-09-07 16:41:54 +0200

    Changed admin password on startup

---
+++ src/main/resources/db/migration/V2__Add_Timestamps_To_Users.sql
@@ -0,0 +1,17 @@
+
+ALTER TABLE users ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE users ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
