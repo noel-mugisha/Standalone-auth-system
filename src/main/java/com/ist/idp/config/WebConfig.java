d71ced0c79828750b96a285ff31b5ba9ca678095
Noel <noelmugisha332@gmail.com>
2025-09-08 22:48:29 +0200

    Setting up environment variables for deployment

--- src/main/java/com/ist/idp/config/WebConfig.java
+++ src/main/java/com/ist/idp/config/WebConfig.java
@@ -1,16 +1,21 @@
 package com.ist.idp.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500", "null")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
