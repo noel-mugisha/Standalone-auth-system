c8e5f52657169834e55e61aabba3b194e046493a
Noel <noelmugisha332@gmail.com>
2025-09-10 08:58:49 +0200

    Email verification settings

--- src/main/java/com/ist/idp/service/EmailService.java
+++ src/main/java/com/ist/idp/service/EmailService.java
@@ -2,6 +2,7 @@ package com.ist.idp.service;
 
 import com.ist.idp.exceptions.OtpException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
@@ -24,6 +26,7 @@ public class EmailService {
             message.setText("Your OTP for email verification is: " + otp + "\nIt is valid for 10 minutes.");
             mailSender.send(message);
         } catch (Exception e) {
+            log.error("Failed to send OTP email to {}. Error: {}", to, e.getMessage(), e);
             throw new OtpException("Failed to send OTP email.");
         }
     }
